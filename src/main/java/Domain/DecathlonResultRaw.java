47eae41f4bec790774323efd100e2c85a0efebbe
idontcare <e.balkunas@gmail.com>
2018-10-15 14:32:28 +0300

    Initial commit -     Added initial gradle config.     Added main domain classes.

---
+++ src/main/java/Domain/DecathlonResultRaw.java
@@ -0,0 +1,108 @@
+package Domain;
+
+public class DecathlonResultRaw {
+
+    private String AthleteName;
+
+    private Double HundredMetersSprintTime;
+    private Double LongJumpDistance;
+    private Double ShotPutDistance;
+    private Double HighJumpDistance;
+    private Double FourHundredMeterRunTime;
+    private Double HurdlesTime;
+    private Double DiscusThrowDistance;
+    private Double PoleVaultDistance;
+    private Double JavelinThrowDistance;
+    private Double OneAndHalfKilometerRunTime;
+
+
+    public DecathlonResultRaw(
+            String athleteName,
+            Double hundredMetersSprintTime,
+            Double longJumpDistance,
+            Double shotPutDistance,
+            Double highJumpDistance,
+            Double fourHundredMeterRunTime,
+            Double hurdlesTime,
+            Double discusThrowDistance,
+            Double poleVaultDistance,
+            Double javelinThrowDistance,
+            Double oneAndHalfKilometerRunTime
+    ) {
+        AthleteName = athleteName;
+        HundredMetersSprintTime = hundredMetersSprintTime;
+        LongJumpDistance = longJumpDistance;
+        ShotPutDistance = shotPutDistance;
+        HighJumpDistance = highJumpDistance;
+        FourHundredMeterRunTime = fourHundredMeterRunTime;
+        HurdlesTime = hurdlesTime;
+        DiscusThrowDistance = discusThrowDistance;
+        PoleVaultDistance = poleVaultDistance;
+        JavelinThrowDistance = javelinThrowDistance;
+        OneAndHalfKilometerRunTime = oneAndHalfKilometerRunTime;
+    }
+
+    public String getAthleteName() {
+        return AthleteName;
+    }
+
+    public Double getHundredMetersSprintTime() {
+        return HundredMetersSprintTime;
+    }
+
+    public Double getLongJumpDistance() {
+        return LongJumpDistance;
+    }
+
+    public Double getShotPutDistance() {
+        return ShotPutDistance;
+    }
+
+    public Double getHighJumpDistance() {
+        return HighJumpDistance;
+    }
+
+    public Double getFourHundredMeterRunTime() {
+        return FourHundredMeterRunTime;
+    }
+
+    public Double getHurdlesTime() {
+        return HurdlesTime;
+    }
+
+    public Double getDiscusThrowDistance() {
+        return DiscusThrowDistance;
+    }
+
+    public Double getPoleVaultDistance() {
+        return PoleVaultDistance;
+    }
+
+    public Double getJavelinThrowDistance() {
+        return JavelinThrowDistance;
+    }
+
+    public Double getOneAndHalfKilometerRunTime() {
+        return OneAndHalfKilometerRunTime;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("{");
+        builder.append(String.format("Athelete's name: %s, ", AthleteName));
+        builder.append(String.format("Sprint time: %f, ", HundredMetersSprintTime));
+        builder.append(String.format("Long jump: %f, ", LongJumpDistance));
+        builder.append(String.format("Shot put: %f, ", ShotPutDistance));
+        builder.append(String.format("High jump: %f, ", HighJumpDistance));
+        builder.append(String.format("400m run: %f, ", FourHundredMeterRunTime));
+        builder.append(String.format("110m hurdles: %f, ", HurdlesTime));
+        builder.append(String.format("Discus throw: %f, ", DiscusThrowDistance));
+        builder.append(String.format("Pole vault: %f, ", PoleVaultDistance));
+        builder.append(String.format("Javelin throw: %f, ", JavelinThrowDistance));
+        builder.append(String.format("1500m run: %f, ", OneAndHalfKilometerRunTime));
+        builder.append("}");
+
+        return builder.toString();
+    }
+}
